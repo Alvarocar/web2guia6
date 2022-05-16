88f0826eb76f596666fda6e2f7d2e6f64415cd7e
Alvaro Cardozo <alvaro_car6@hotmail.com>
2022-05-15 21:48:14 -0500

    v1

---
+++ src/main/java/dev/alvaro/web2guia6/Web2guia6Application.java
@@ -0,0 +1,13 @@
+package dev.alvaro.web2guia6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2guia6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2guia6Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alvaro/web2guia6/Web2guia6ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alvaro.web2guia6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2guia6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
